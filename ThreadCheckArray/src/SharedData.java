2447151c1192a55b9ef8d08ae4266ab4aa682de0
hamza <cr7h@DESKTOP-SC11NOT>
2024-01-21 13:00:13 +0200

    ....

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,41 +1,72 @@
+import java.util.ArrayList;
+
+/**
+ * @author cr7h
+ *
+ */
 public class SharedData 
 {
-	private int [] array;
+	/**
+	 * 
+	 */
+	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
 	
+	/**
+	 * @param array
+	 * @param b
+	 */
 	public SharedData(int[] array, int b) {
 		
-		this.array = array;
+		this.array =new ArrayList<>();
 		this.b = b;
 	}
 
+	/**
+	 * @return
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/**
+	 * @param winArray
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	/**
+	 * @return
+	 */
+	public ArrayList<Integer> getArray()  
 	{
 		return array;
 	}
 
+	/**
+	 * @return
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * @return
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**ىؤلىلبى
+	 * @param flag  ابابابؤاؤ
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
